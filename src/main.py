74d072276db00b403c07eca988e37b94a23ebd65
Mathieu Bouchet <mathieu.bouchet@epita.fr>
2021-09-29 20:22:57 +0200

    Add !count

--- src/main.py
+++ src/main.py
@@ -1,11 +1,15 @@
 import os
 from discord.ext import commands
+from discord import Intents
 from dotenv import load_dotenv
 
 load_dotenv()
 
+intents = Intents.all()
+
 bot = commands.Bot(
     command_prefix="!",  # Change to desired prefix
+    intents=intents,
     case_insensitive=True  # Commands aren't case-sensitive
 )
 
@@ -24,6 +28,13 @@ async def pong(ctx):
 async def name(ctx):
     await ctx.send(ctx.author)
 
+@bot.command()
+async def count(ctx):
+    statuslist = { "online": 0, "offline": 0, "idle": 0, "dnd": 0 }
+    for member in ctx.guild.members:
+        statuslist[member.raw_status] = statuslist[member.raw_status] + 1
+    
+    await ctx.send(f"{statuslist['online']} are online, {statuslist['idle']} are idle, {statuslist['dnd']} are dnd and {statuslist['offline']} are off")
 
 
 token = os.getenv("TOKEN_BOT")
